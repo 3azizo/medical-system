07e94619cbab94662db14a11b191b180fadc39f5
3azizo <mohamedreda7494591@gmail.com>
2025-06-11 01:23:10 +0300

    deleted

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -26,12 +26,11 @@ export const addLab =async (req, res) => {
 // Delete a lab (permanent)
 export const deleteLab = async (req, res) => {
   try {
-    const lab = await User.findById(req.params.id);
-    if (!lab || lab.role !== 'medical_lab') {
+    const lab = await Lab.findById(req.params.id);
+    await lab.deleteOne();
+    if (!lab) {
       return res.status(404).json({ msg: 'Lab not found' });
     }
-
-    await User.findByIdAndDelete(req.params.id);
     res.status(200).json({ msg: 'Lab deleted successfully' });
   } catch (err) {
     res.status(500).json({ msg: 'Server Error' });
